776ec590492349fd287e907f48ee3f4871577293
MDAM182 <mdam2345@gmail.com>
2021-06-17 09:54:21 -0400

    Update event listner added to DOM

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
   const endPoint = "http://localhost:3000/api/v1/programs"
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("DOM is Loaded");
+  console.log("DOM is Loaded");
   getPrograms()
-  const createProgramForm = document.querySelector("#create-program-form" )
+  const app = new App();
+  app.attachEventListeners();
+
+  const createProgramForm = document.querySelector("#create-program-form")
   createProgramForm.addEventListener("submit", (e) => createFormHandler(e))
-})
+
+  const programContainer = document.querySelector('#program-container')
+      programContainer.addEventListener('click', e => {
+        const id = parseInt(e.target.closest('[data-id]').dataset.id)
+        const program = Program.findById(id)
+        console.log(program)
+        document.querySelector('#update-program').innerHTML += program.renderPatchForm()
+
+
+    })
+  })
 
   function getPrograms() {
     fetch(endPoint)
     .then(res => res.json())
     .then(programs => {
       programs.data.forEach(program => {
-
-
-
-    const newProgram = new Program(program, program.attributes)
-         document.querySelector('#program-container').innerHTML += newProgram.render()
+      let newProgram = new Program(program, program.attributes)
+         document.querySelector('#program-container').innerHTML += newProgram.renderProgramCard()
     })
    })
   }
-function postFetch(title, favorite_quote, image_url, media_type, genre_id ){
-  const bodyData = {title, favorite_quote, image_url, media_type, genre_id}
-  fetch(endPoint, {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(bodyData)
-  })
-  .then(res => res.json())
-  .then(program => {
-
-      const newProgram = new Program(program.data, program.data.attributes)
-     document.querySelector('#program-container').innerHTML += newProgram.render()
-  })
-}
 
 function createFormHandler(e){
   e.preventDefault()
@@ -45,6 +41,51 @@ function createFormHandler(e){
 
   postFetch(titleInput, quoteInput, imgInput, mediaTypeInput, genreInput)
 
-}
+ }
 
-  // .catch(err => console.log(err))
+ function postFetch(title, favorite_quote, image_url, media_type, genre_id ){
+   const bodyData = {title, favorite_quote, image_url, media_type, genre_id}
+   fetch(endPoint, {
+     method: "POST",
+     headers: {"Content-Type": "application/json"},
+     body: JSON.stringify(bodyData)
+   })
+   .then(res => res.json())
+   .then(program => {
+
+       const newProgram = new Program(program.data, program.data.attributes)
+      document.querySelector('#program-container').innerHTML += newProgram.renderProgramCard()
+   })
+ }
+
+ // function updateFormHandler(e) {
+ //   e.preventDefault();
+ //   const id = parseInt(e.target.dataset.id);
+ //   const program = Program.findById(id);
+ //   const title = e.target.querySelector('#input-title').value;
+ //   const quote = e.target.querySelector('#input-quote').value;
+ //   const image_url = e.target.querySelector('#input-url').value;
+ //   const media_type = e.target.querySelector('#media_type').value
+ //   const genre_id = parseInt(e.target.querySelector('#genres').value);
+ //   patchProgram(program, title, quote, image_url, media_type, genre_id)
+ // }
+ //
+ // function patchProgram(title, favorite_quote, image_url, media_type, genre_id) {
+ //   program = Program.findById(id)
+ //   const bodyJSON = {title, favorite_quote, image_url, media_type, genre_id}
+ //   fetch(`http://localhost:3000/api/v1/programs/${program.id}`, {
+ //     method: "PATCH",
+ //     headers: {'Content-Type': 'application/json'},
+ //     body: JSON.stringify(bodyJSON),
+ //   })
+ //     .then(res => res.json())
+ //     .then(updatedProgram => {
+ //       console.log(updatedProgram)
+ //       // debugger
+ //         // const program = Program.findById(updatedProgram.data.id);
+ //         // program.update(updatedProgram.data.attributes);
+ //         // document.querySelector('#program-container').innerHTML = '';
+ //         // Program.all.forEach(program => document.querySelector('#program-container').innerHTML += program.renderProgramCard());
+ //         // document.querySelector('#update-program').innerHTML = '';
+ //    })
+ //  }
