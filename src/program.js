539d79e4057e6ef84bd5451107c5b678f8314e12
MDAM182 <mdam2345@gmail.com>
2021-06-17 09:55:19 -0400

    patch form created, update method created

--- src/program.js
+++ src/program.js
@@ -9,8 +9,9 @@ class Program {
     Program.all.push(this)
   }
 
-  render(){
 
+
+  renderProgramCard(){
     return `
 
         <div data-id=${this.id}>
@@ -19,10 +20,61 @@ class Program {
           <p>"${this.favorite_quote}"</p>
           <p>${this.media_type}</p>
           <p>${this.genre.name}</p>
-          <button data-id=${this.id}>edit</button>
+          <button data-id=${this.id}>Edit</button>
         </div>
         <br><br>`
     }
+
+    static findById(id) {
+       return this.all.find(program => program.id == id)
+   }
+
+   renderPatchForm() {
+       return `
+         <form data-id=${this.id} >
+           <h3>Edit Program</h3>
+
+          <input id='input-title' type="text" name="title" value="" placeholder="${this.title}" class="input-text">
+         <br><br>
+
+         <textarea id='input-quote' name="favorite_quote" rows="8" cols="80" value="" placeholder="${this.favorite_quote}"></textarea>
+         <br><br>
+
+         <input id='input-url' type="text" name="image" value="" placeholder="${this.image_url}" class="input-text">
+         <br><br>
+
+         <select id="media_type" name="media_type" value="${this.media_type}">
+           <option value="Film">Film</option>
+           <option value="TV">TV</option>
+         </select>
+
+           <select id="genres" name="genres" value="${this.genre.name}">
+             <option value="1">Comedy</option>
+             <option value="2">Sci-Fi</option>
+             <option value="3">Action</option>
+             <option value="4">Horror</option>
+             <option value="5">Drama</option>
+             <option value="6">Fantasy</option>
+           </select>
+           <br><br>
+
+        <input id= 'edit-button' type="submit" name="submit" value="Submit Edit" class="submit">
+     </form>
+       `
+
+     }
+     //
+
+     update(title, favorite_quote, image_url, media_type, genre) {
+          this.title = title;
+          this.favorite_quote = favorite_quote;
+          this.image_url = image_url;
+          this.media_type = media_type;
+          this.genre = genre;
+
+      }
+
+
 }
 
 Program.all = []
